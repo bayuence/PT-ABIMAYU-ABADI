e0cb78777cba5de6a8a26104117c7f6664814b7c
bayuence <bayuence@gmail.com>
2026-05-13 05:19:30 +0700

    Add initial values for contact info CMS

--- schemaTypes/contactInfo.ts
+++ schemaTypes/contactInfo.ts
@@ -4,6 +4,13 @@ export default defineType({
   name: 'contactInfo',
   title: 'Informasi Kontak',
   type: 'document',
+  initialValue: {
+    title: 'Mulai Proyek Anda Bersama Kami',
+    phones: ['021-22708806', '0878-2516-7150', '0856-7135-400'],
+    emails: ['ops.abimanyu@gmail.com'],
+    address: 'Gd. AD Premier Lt.17 Suite 04B, Jl. TB Simatupang, Jakarta Selatan 12550',
+    whatsappNumber: '6287825167150',
+  },
   fields: [
     defineField({
       name: 'title',
